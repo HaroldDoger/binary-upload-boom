3e5b01ea8b3cb373f1dcdd695943df10b05db0b3
harold doger <harolddoger@gmail.com>
2022-09-15 01:59:36 -0400

    small bar changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,12 +45,9 @@ exports.postLogin = (req, res, next) => {
 };
 
 exports.logout = (req, res) => {
-  req.logout();
-  req.session.destroy((err) => {
-    if (err)
-      console.log("Error : Failed to destroy the session during logout.", err);
-    req.user = null;
-    res.redirect("/");
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
   });
 };
 
